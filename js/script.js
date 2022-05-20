a5b1fe29383cea84b6424cf0f05046ef8f37fdf8
Joseph Barbone <JoeBarbone@aol.com>
2022-05-19 23:53:44 -0400

    everything works, just clean it up and make it look nicer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-
 var btnSubmitEl = document.querySelector("#submit");
-
-
-
-
-
 
 var getMovie = function() {
     var requestUrl = 'http://www.omdbapi.com/?apikey=94f7ec29&t='
@@ -23,18 +17,19 @@ var getMovie = function() {
             document.getElementById("year").innerHTML = data.Year;
             document.getElementById("rating").innerHTML = data.Rated;
             document.getElementById("actors").innerHTML = data.Actors;
-            document.getElementById("poster").innerHTML = "<img src='" + data.Poster + "' />";
+            document.getElementById("poster").innerHTML = "<img class='img-fluid' src='" + data.Poster + "' />";
             document.getElementById("plot").innerHTML = data.Plot;
+
+            document.getElementById("movie-info").style.display = "none";
+
+            document.getElementById("movie-title").value = "";
+            document.getElementById("movie-title").focus();
+
         
     })
   
 
 }
-
-
-
-
-
 
 
 
