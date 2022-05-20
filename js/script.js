46c3a0c23606fa5819b9975e1ed771c3d8c3bfd1
Joseph Barbone <joebarbone@aol.com>
2022-05-19 20:39:46 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+var btnSubmitEl = document.querySelector("#submit");
+
+
+
+
+
+
+var getMovie = function() {
+    var requestUrl = 'http://www.omdbapi.com/?apikey=94f7ec29&t='
+    var mt = document.getElementById("movie-title").value;
+    requestUrl = requestUrl + mt;
+
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+            console.log(data);
+            
+            document.getElementById("title").innerHTML = data.Title;
+            document.getElementById("year").innerHTML = data.Year;
+            document.getElementById("rating").innerHTML = data.Rated;
+            document.getElementById("actors").innerHTML = data.Actors;
+            document.getElementById("poster").innerHTML = "<img src='" + data.Poster + "' />";
+        
+        
+    })
+  
+
+}
+
+
+
+
+
+
+
+
+
+btnSubmitEl.addEventListener("click", getMovie);
