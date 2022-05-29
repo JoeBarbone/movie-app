67db37c08d74436609cdf5a9062cc5833cdef3b0
Joseph Barbone <JoeBarbone@aol.com>
2022-05-29 16:34:04 -0400

    completed viewing options, but still need to format

--- js/script.js
+++ js/script.js
@@ -36,11 +36,7 @@ var demoRatingsHeaderContainerEl = document.querySelector("#demo-ratings-header"
 var demoRatingsHeaderEl = document.createElement("p");
 
 
-// displays viewing options
-var viewingOptionsHeadingContainerEl = document.querySelector("#view-options-heading");
-var viewRentOptionsHeadingContainerEl = document.querySelector("#rent-options-heading");
-var viewBuyOptionsHeadingContainerEl = document.querySelector("#buy-options-heading");
-var viewSubOptionsHeadingContainerEl = document.querySelector("#sub-options-heading");
+
 
 
 
@@ -137,27 +133,27 @@ var getRatings = function(imdbid) {
     // data.errorMessage 
     
 
-    // fetch("xhttps://imdb-api.com/en/API/Ratings/k_e8wpwz9e/" + imdbid)
-    //     .then(function(response) {
-    //         return response.json();
-    //     })
-    //     .then(function(data) {
-    //         console.log(data);
+     fetch("https://imdb-api.com/en/API/Ratings/k_e8wpwz9e/" + imdbid)
+         .then(function(response) {
+             return response.json();
+         })
+         .then(function(data) {
+             console.log(data);
 
-            // commented out for testing
-            //var title = data.fullTitle;
-            //var imdbid = data.imDbId;
-            //var imdbrating = data.imDb;
-            //var rt = data.rottenTomatoes;
-            //var tmdb = data.theMovieDb;
+            // comment out for testing
+            var title = data.fullTitle;
+            var imdbid = data.imDbId;
+            var imdbrating = data.imDb;
+            var rt = data.rottenTomatoes;
+            var tmdb = data.theMovieDb;
 
             // set for testing
-            var title = "Killer Klowns From Outer Space";
+            //var title = "Killer Klowns From Outer Space";
             // var imdbid = "tt012968";
-            var imdbid = "tt10954652";
-            var imdbrating = "6.8";
-            var rt = "68.9";
-            var tmdb = "6.8";
+            //var imdbid = "tt10954652";
+            //var imdbrating = "6.8";
+            //var rt = "68.9";
+            //var tmdb = "6.8";
             
 
             //console.log("<h1>Movie Information</h1><br />Title: " + title + "\n" + "IMDBID: " + imdbid + "\n" + "Spoiled Potatoes Rating: " + rt + "/100 \n" + "The Movie DB Rating: " + tmdb + "/10");
@@ -174,31 +170,32 @@ var getRatings = function(imdbid) {
             // displays ratings info in top of left-container 
             ratingsContainerEl.appendChild(ratingsEl);
             getDemographicRatings(imdbid);
-        //})
+        })
 }
 
 
 
 var getDemographicRatings = function(imdbid) {
-    // fetch("xhttps://imdb-api.com/en/API/UserRatings/k_e8wpwz9e/" + imdbid)
-    //      .then(function(response) {
-    //          return response.json();
-    //      })
-    //      .then(function(data) {
-    //          console.log(data);
+     fetch("https://imdb-api.com/en/API/UserRatings/k_e8wpwz9e/" + imdbid)
+          .then(function(response) {
+              return response.json();
+          })
+          .then(function(data) {
+              console.log(data);
 
-            // commented out for testing
-            // var under18 = data.demographicAll.agesUnder18.rating;
-            // var ages18To29 = data.demographicAll.ages18To29.rating;
-            // var ages30To44 = data.demographicAll.ages30To44.rating;
-            // var agesOver45 = data.demographicAll.agesOver45.rating;
-            // var allAges = data.demographicAll.allAges.rating;
+            // comment out for testing
+            var under18 = data.demographicAll.agesUnder18.rating;
+            var ages18To29 = data.demographicAll.ages18To29.rating;
+            var ages30To44 = data.demographicAll.ages30To44.rating;
+            var agesOver45 = data.demographicAll.agesOver45.rating;
+            var allAges = data.demographicAll.allAges.rating;
 
-            var under18 = "6.8";
-            var ages18To29 = "6.8";
-            var ages30To44 = "6.8";
-            var agesOver45 = "6.8";
-            var agesAll = "6.8";
+            // uncomment for testing
+            //var under18 = "6.8";
+            //var ages18To29 = "6.8";
+            //var ages30To44 = "6.8";
+            //var agesOver45 = "6.8";
+            //var agesAll = "6.8";
 
             
             var numberFormatter = Intl.NumberFormat("en-US");
@@ -216,24 +213,27 @@ var getDemographicRatings = function(imdbid) {
                 ages18To29 + "<br />" + 
                 ages30To44 + "<br />" + 
                 agesOver45  + "<br /><br />" + 
-                agesAll;
+                allAges;
 
             userRatingContainerEl.appendChild(userRatingEl);
 
             // sets user ratings votes
-            // commented out for testing
-            // var agesUnder18Votes = data.demographicAll.agesUnder18.votes;
-            // var ages18To29Votes = data.demographicAll.ages18To29.votes;
-            // var ages30To44Votes = data.demographicAll.ages30To44.votes;
-            // var agesOver45Votes = data.demographicAll.agesOver45.votes;
-            // var agesAllVotes = data.demographicAll.allAges.votes;
+            // comment out for testing
+            var agesUnder18Votes = data.demographicAll.agesUnder18.votes;
+            var ages18To29Votes = data.demographicAll.ages18To29.votes;
+            var ages30To44Votes = data.demographicAll.ages30To44.votes;
+            var agesOver45Votes = data.demographicAll.agesOver45.votes;
+            var allAgesVotes = data.demographicAll.allAges.votes;
 
-            // setup for testing
-            var agesUnder18Votes = "684";
-            var ages18To29Votes = "32863";
-            var ages30To44Votes = "23993";
-            var agesOver45Votes = "32234";
-            var agesAllVotes = "83235";
+            
+            
+            
+            // uncomment this setup for testing
+            //var agesUnder18Votes = "684";
+            //var ages18To29Votes = "32863";
+            //var ages30To44Votes = "23993";
+            //var agesOver45Votes = "32234";
+            //var agesAllVotes = "83235";
 
             votesEl.innerHTML = "Votes<br /><br />" + 
                 // numberFormatter.format(data.demographicAll.agesUnder18.votes) + "<br />" +
@@ -242,11 +242,12 @@ var getDemographicRatings = function(imdbid) {
                 // numberFormatter.format(data.demographicAll.agesOver45.votes) + "<br /><br />" +
                 // numberFormatter.format(data.demographicAll.allAges.votes);
 
+                
                 numberFormatter.format(agesUnder18Votes) + "<br />" +
                 numberFormatter.format(ages18To29Votes) + "<br />" +
                 numberFormatter.format(ages30To44Votes) + "<br />" +
                 numberFormatter.format(agesOver45Votes) + "<br /><br />" +
-                numberFormatter.format(agesAllVotes);
+                numberFormatter.format(allAgesVotes);
             
             votesContainerEl.appendChild(votesEl);
 
@@ -254,7 +255,7 @@ var getDemographicRatings = function(imdbid) {
             
             // demoRatingsEl.innerHTML = ;
 
-            // commented out for testing
+            // used for testing
             //for (var i=0; i < data.ratings.length; i++) {
 
             //var imdbid = data.imDbId;
@@ -263,7 +264,7 @@ var getDemographicRatings = function(imdbid) {
             //}
             
 
-        //})
+        })
         getViewingOptions(imdbid);
 }
 
@@ -271,19 +272,44 @@ var getDemographicRatings = function(imdbid) {
 
 var getViewingOptions = function(imdbid) {
 
-    //var viewingOptionsHeaderEl = document.createElement("div");
-    // var viewRentContainerEl = document.querySelector("#rent-options");
-    // var viewBuyContainerEl = document.querySelector("#buy-options");
-    // var viewSubContainerEl = document.querySelector("#sub-options");
-
-    var viewRentHeadingEl = document.createElement("div");
-    var viewBuyHeadingEl = document.createElement("div");
-    var viewSubHeadingEl = document.createElement("div");
     
-    viewingOptionsHeaderEl.innerHTML = "<p class='title is-1'>Viewing Options</p>";
+
+    // setting IDs to display headings
+    var viewOptionsHeadingContainerEl = document.querySelector("#view-options-heading");
+    var viewRentOptionsHeadingContainerEl = document.querySelector("#rent-options-heading");
+    var viewBuyOptionsHeadingContainerEl = document.querySelector("#buy-options-heading");
+    var viewSubOptionsHeadingContainerEl = document.querySelector("#sub-options-heading");
+
+    // creating elements to assign options heading title, IE: Rental, Purchase, Subscription
+    var viewOptionsHeadingEl = document.createElement("p");
+    var viewRentHeadingEl = document.createElement("p");
+    var viewBuyHeadingEl = document.createElement("p");
+    var viewSubHeadingEl = document.createElement("p");
+
+    // setting heading data
+    viewOptionsHeadingEl.innerHTML = "Viewing Options";
     viewRentHeadingEl.innerHTML = "Rental Options";
     viewBuyHeadingEl.innerHTML = "Purchase Options";
     viewSubHeadingEl.innerHTML = "Subscription Options";
+
+
+    
+    // setting containers to display data
+    var viewRentContainerEl = document.querySelector("#rent-viewing-options");
+    var viewBuyContainerEl = document.querySelector("#buy-viewing-options");
+    var viewSubContainerEl = document.querySelector("#sub-viewing-options");
+
+    
+    
+    
+    
+
+
+    
+    
+
+
+
 
     fetch("https://api.watchmode.com/v1/title/" + imdbid + "/sources/?apiKey=QKlb5uqJqrs4TSLGQHRsUwAMgKndbhqCOUaIyCW0")
         .then(function(response) {
@@ -295,13 +321,13 @@ var getViewingOptions = function(imdbid) {
     .then(function(data) {
         console.log(data);
 
-        // new display
-        // viewingOptionsHeaderEl.innerHTML = "what the fuck";
-        viewingOptionsHeadingContainerEl.appendChild(viewingOptionsHeaderEl);
-        viewRentContainerEl.appendChild(viewRentHeadingEl);
-        viewBuyContainerEl.appendChild(viewBuyHeadingEl);
-        viewSubContainerEl.appendChild(viewSubHeadingEl);
+        // adding data to container element to display
+        viewOptionsHeadingContainerEl.appendChild(viewOptionsHeadingEl);
+        viewRentOptionsHeadingContainerEl.appendChild(viewRentHeadingEl);
+        viewBuyOptionsHeadingContainerEl.appendChild(viewBuyHeadingEl);
+        viewSubOptionsHeadingContainerEl.appendChild(viewSubHeadingEl);
         
+        var iconPath = "";
     
         for (var i=0; i < data.length; i++) {
     
@@ -312,32 +338,62 @@ var getViewingOptions = function(imdbid) {
             var type = data[i].type;
     
             console.log(format);
-            if (format === "HD") {
+            if ((name === "Google Play" || name === "Hulu" || name === "Netflix" || name === "Amazon" || name === "YouTube" || name === "DirecTV On Demand" || name === "Spectrum On Demand") && format === "HD") {
                 
+                switch (name) {
+                    
+                    case "Netflix":
+                        iconPath = "https://cdn.watchmode.com/provider_logos/netflix_100px.png";
+                        break;
+
+                    case "Hulu":
+                    iconPath = "https://cdn.watchmode.com/provider_logos/hulu_100px.png";
+                    break;
+
+                    case "Amazon":
+                    iconPath = "https://cdn.watchmode.com/provider_logos/prime_video_100px.png";
+                    break;
+
+                    case "YouTube":
+                    iconPath = "https://cdn.watchmode.com/provider_logos/youtubePremium_100px.png";
+                    break;
+
+                    case "DirecTV On Demand":
+                    iconPath = "https://cdn.watchmode.com/provider_logos/442_autogenerated.png";
+                    break;
+
+                    case "Spectrum On Demand":
+                    iconPath = "https://cdn.watchmode.com/provider_logos/443_autogenerated.png";
+                    break;
+                }
+
+
+
+
                 switch (type) {
                     case "rent":
                         
                         // this has been an issue in the past, I keep forgetting to create the element inside the for loop
                         
-                        var viewingOptionsEl = document.createElement("p");
-                        viewingOptionsEl.innerHTML = "Service Provider: " + name + "<br />" + "Price: " + price + "<br />" + "Format: " + format + "<br />" + "Type: " + type + "<br />" + "Web Link: " + web + "<br /><br />";
-                        viewingOptionsContainerEl.appendChild(viewingOptionsEl);
+                        var viewRentEl = document.createElement("p");
+                        viewRentEl.innerHTML = "<img src='" + iconPath + "' />" + "&emsp;" + name + "&emsp;" + "Price: " + price + "&emsp;" + "Format: " + format + "&emsp;" + "Type: " + type + "&emsp;" + "<br /><br />";
+                        viewRentContainerEl.appendChild(viewRentEl);
                         break;
 
                     case "buy":
                     
                         // this has been an issue in the past, I keep forgetting to create the element inside the for loop
-                        var viewingOptionsEl = document.createElement("p");
-                        viewingOptionsEl.innerHTML = "Service Provider: " + name + "<br />" + "Price: " + price + "<br />" + "Format: " + format + "<br />" + "Type: " + type + "<br />" + "Web Link: " + web + "<br /><br />";
-                        viewingOptionsContainerEl.appendChild(viewingOptionsEl);
+                        var viewBuyEl = document.createElement("p");
+                        viewBuyEl.innerHTML = "<img src='" + iconPath + "' />" + "&emsp;" + name + "&emsp;" + "Price: " + price + "&emsp;" + "Format: " + format + "&emsp;" + "Type: " + type + "&emsp;" +  "<br /><br />";
+                        viewBuyContainerEl.appendChild(viewBuyEl);
                         break;
 
                     case "sub":
                 
                         // this has been an issue in the past, I keep forgetting to create the element inside the for loop
-                        var viewingOptionsEl = document.createElement("p");
-                        viewingOptionsEl.innerHTML = "Service Provider: " + name + "<br />" + "Price: " + price + "<br />" + "Format: " + format + "<br />" + "Type: " + type + "<br />" + "Web Link: " + web + "<br /><br />";
-                        viewingOptionsContainerEl.appendChild(viewingOptionsEl);
+                        var viewSubEl = document.createElement("p");
+                        viewSubEl.innerHTML = "<img src='" + iconPath + "' />" + "&emsp;" + name + "&emsp;" + "&emsp;" + "Format: " + format + "&emsp;" + "Type: " + type + "&emsp;" + "<br /><br />";
+                        viewSubContainerEl.appendChild(viewSubEl);
                         break;
 
                     
